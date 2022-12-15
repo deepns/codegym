a5dcd172f1529b46391c786276a384d36eb4f478
Deepan Seeralan <57165211+deepns@users.noreply.github.com>
2022-12-15 07:42:43 -0500

    python - threading - learning continues

--- python/pydockshm/ex_threading.py
+++ python/pydockshm/ex_threading.py
@@ -2,21 +2,37 @@ import threading
 import random
 import time
 
-NUM_THREADS=10
+# TODO
+# - [ ] split this into two examples
+
+NUM_THREADS=1
 
 def thread_func(tid):
-    print("starting thread:", tid)
+    print("starting thread:{}, name:{}".format(tid, threading.get_ident()))
+    
     # do some work.
-    time.sleep(random.randint(1, 10))
+    sleep_time = random.randint(1, 10)
+    print("sleeping for {} seconds".format(sleep_time))
+    time.sleep(sleep_time)
+
     print("done thread:", tid)
 
 # Start the worker threads
 threads = []
 for i in range(NUM_THREADS):
+    # starting a non-daemon thread
+    # threading module has a shutdown function that runs on program exit
+    # it calls join() on any non-daemon thread that is still active
     t = threading.Thread(target=thread_func, args=(i, ))
     threads.append(t)
     t.start()
 
+print("main: done")
+
+# not needed in case of non-daemon threads, if the intention is wait at the end.
+# if need to wait for the threads to finish before main ends, then call join()
+# where needed
+
 # Wait until all threads are complete
-for thread in threads:
-    thread.join()
+# for thread in threads:
+#     thread.join()
