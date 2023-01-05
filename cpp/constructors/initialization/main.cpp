7a942f75ac67c29631a6fac8e01eeaa31617f878
Deepan Seeralan <57165211+deepns@users.noreply.github.com>
2023-01-04 23:26:12 -0500

    cpp - explicit constructors

--- cpp/constructors/initialization/main.cpp
+++ cpp/constructors/initialization/main.cpp
@@ -1,19 +1,23 @@
 #include <iostream>
 
-class Room {
+class Room
+{
 public:
     // Default constructor
-    Room() {
+    Room()
+    {
+        m_width = m_length = m_height = 0;
         std::cout << "Constructing room():"
                   << "width=" << m_width << " "
                   << "height=" << m_height << " "
-                  << "length=" << m_length << std::endl;                  
+                  << "length=" << m_length << std::endl;
     }
 
     // using member initializer
     // members are initialized before the constructor body runs
-    Room(int dimension) :
-        m_height(dimension), m_width(dimension), m_length(dimension) {
+    // can members be initialized in any order?
+    Room(int dimension) : m_height(dimension), m_width(dimension), m_length(dimension)
+    {
         std::cout << "Constructing room(dimension): "
                   << "width=" << m_width << " "
                   << "height=" << m_height << " "
@@ -21,23 +25,23 @@ public:
     }
 
     // overloaded constructors
-    Room(int width, int length, int height) : 
-        m_height(height), m_width(width), m_length(length) {
+    Room(int width, int length, int height) : m_height(height), m_length(length), m_width(width)
+    {
         std::cout << "Constructing room(height, width, length): "
                   << "width=" << m_width << " "
                   << "height=" << m_height << " "
                   << "length=" << m_length << std::endl;
     }
 
-    int area() {
+    int area()
+    {
         return m_width * m_height * m_length;
     }
 
 private:
-    int m_width {0};
-    int m_height {0};
-    int m_length {0};
-
+    int m_width;
+    int m_height;
+    int m_length;
 };
 
 int main()
