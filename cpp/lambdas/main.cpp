4e127f30cdfabff61950b75b1c81a26fa37dc475
Deepan Seeralan <57165211+deepns@users.noreply.github.com>
2023-03-15 11:03:47 -0400

    c++ - some more on lambda

--- cpp/lambdas/main.cpp
+++ cpp/lambdas/main.cpp
@@ -6,9 +6,22 @@
 //
 // Syntax:
 //
-//  [ capture clause ] ( parameters ) -> return_type {
+//  [ capture clause ] ( parameter-list ) mutable throw() -> return_type {
 //      // function body
 // }
+//
+// Capture clause - capture values in the outer scope by reference or by value
+//  & - to capture by reference
+//  = - to capture by value
+//  can have mix of capture by reference and capture by value
+//  [&] - capture all by reference
+//  [&, var_a] - capture all by reference and var_a by value
+//  [=, &var_a] - capture all by value and var_a by reference
+// parameter-list - optional
+// mutable - optional
+// exception sepecfications - optional
+// return_type - optional
+//
 
 int main()
 {
@@ -27,11 +40,20 @@ int main()
 
     auto pi = 3.14;
 
-    // another lambda that uses the variables from the outer scope
+    // another lambda that captures all the variables from the outer scope
+    // generally not advisable to capture all the variables. capture only
+    // what is necessary
     auto area = [&](int r)
     {
         return pi * r * r;
     };
+
+    int r = 10;
+    // capture r by reference and rest by value
+    auto dia = [=, &r]() {
+        return 2 * pi * r;
+    };
+    std::cout << "dia()=" << dia() << std::endl;
 
     std::cout << "Calling max lambda: max(10, 20): " << max(10, 20) << std::endl;
     std::cout << "Calling area lambda: area(5): " << area(5) << std::endl;
@@ -45,13 +67,16 @@ int main()
     std::cout << "typeid(colors) = " << typeid(colors).name() << "\n";
 
     auto has_green = std::find(colors.begin(), colors.end(), "green");
-    if (has_green == colors.end()) {
+    if (has_green == colors.end())
+    {
         std::cout << "Can't find green\n";
-    } else {
+    }
+    else
+    {
         std::cout << "Found green!\n";
     }
 
-        // find_if returs an iterator
+    // find_if returs an iterator
     // pass a lambda function to the find_if predicate
     auto has_black = std::find_if(colors.begin(), colors.end(), [](std::string color)
                                   { return color == "black"; });
@@ -70,7 +95,8 @@ int main()
     auto ages_below_5 = std::find_if(ages.begin(), ages.end(), [](int age)
                                      { return age < 5; });
 
-    if (ages_below_5 != ages.end()) {
+    if (ages_below_5 != ages.end())
+    {
         std::cout << "Has ages below 5" << std::endl;
     }
     return 0;
